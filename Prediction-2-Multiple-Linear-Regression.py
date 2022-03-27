045bccb3dabb6486833cac930c51fef2d464f923
alperenalp <alperen.alp045@gmail.com>
2022-03-27 14:39:48 +0300

    Study Backward Elimination

--- Prediction-2-Multiple-Linear-Regression.py
+++ Prediction-2-Multiple-Linear-Regression.py
@@ -83,6 +83,37 @@ regressor.fit(x_train, y_train)
 
 y_pred = regressor.predict(x_test)
 
+boy = s2.iloc[:,3:4].values
+print(boy)
+sol = s2.iloc[:,:3]
+sag = s2.iloc[:,4:]
+
+veri = pd.concat([sol,sag],axis = 1
+                 )
+x_train, x_test, y_train, y_test = train_test_split(veri, boy, test_size=0.33, random_state=0)
+
+r2= LinearRegression()
+r2.fit(x_train,y_train)
+
+y_pred=r2.predict(x_test)
+
+import statsmodels.api as sm
+
+X= np.append(arr = np.ones((22, 1)).astype(int), values = veri, axis = 1)
+
+X_l = veri.iloc[:,[0,1,2,3,4,5]].values
+X_l = np.array(X_l,dtype=float)
+model = sm.OLS(boy, X_l).fit()
+print(model.summary())
 
 
+X_l = veri.iloc[:,[0,1,2,3,5]].values
+X_l = np.array(X_l,dtype=float)
+model = sm.OLS(boy, X_l).fit()
+print(model.summary())
+
+X_l = veri.iloc[:,[0,1,2,3]].values
+X_l = np.array(X_l,dtype=float)
+model = sm.OLS(boy, X_l).fit()
+print(model.summary())
 
