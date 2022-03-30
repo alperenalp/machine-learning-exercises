cd1ad5d980aad25c1c9269fcbf828b9defd8ba87
alperenalp <alperen.alp045@gmail.com>
2022-03-30 16:10:16 +0300

    Update Prediction-4-Support-Vector-Regression.py

--- Prediction-4-Support-Vector-Regression.py
+++ Prediction-4-Support-Vector-Regression.py
@@ -20,46 +20,6 @@ y = veriler.iloc[:,2:]
 X = x.values
 Y = y.values
 
-# linear regression 
-# dogrusal model oluşturma
-from sklearn.linear_model import LinearRegression
-lin_reg= LinearRegression()
-lin_reg.fit(X, Y)
-tahmin_lin = lin_reg.predict(X)
-
-#Görselleştirme
-plt.scatter(X, Y, color="blue")
-plt.plot(X, tahmin_lin, color="red")
-plt.show()
-
-# polynomial regression 
-# dogrusal olmayan (nonlinear) model oluşturma
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=5)
-x_poly = poly_reg.fit_transform(X)
-print(x_poly)
-
-# lineer regresyon kullanarak fit yapma eğitme
-lin_reg2 = LinearRegression()
-lin_reg2.fit(x_poly, y)
-
-# predict'in içine dogrusal sayi yazamayız polinomsal değerler vermek zorundayız
-tahmin_poly = lin_reg2.predict(poly_reg.fit_transform(X))
-
-#Görselleştirme
-plt.scatter(X, Y, color="blue")
-plt.plot(X, tahmin_poly, color="red")
-plt.show()
-
-#tahmin denemeleri
-print("Doğrusal Regresyon 11 ve 6 indexine göre tahmin sonuçları")
-print(lin_reg.predict([[11]]))
-print(lin_reg.predict([[6.6]]))
-
-print("\nPolinomal Regresyon 11 ve 6 indexine göre tahmin sonuçları")
-print(lin_reg2.predict(poly_reg.fit_transform([[11]])))
-print(lin_reg2.predict(poly_reg.fit_transform([[6.6]])))
-
 
 # verilerin olceklenmesi
 from sklearn.preprocessing import StandardScaler
